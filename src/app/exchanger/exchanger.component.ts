f5e8ec31bc297c63a2b2f25755d88665445aea4a
Maksym Maksymyshyn <plazom@gmail.com>
2018-11-11 11:34:52 +0100

    finished template

--- src/app/exchanger/exchanger.component.ts
+++ src/app/exchanger/exchanger.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common/src/pipes/date_pipe';
 
 @Component({
   selector: 'app-exchanger',
@@ -7,18 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExchangerComponent implements OnInit {
   summaries = ['EUR', 'USD', 'GBP', 'AUD', 'CAD', 'JPY'];
+  noError = true;
+  baseCurrency = "EUR";
+  date = '';
+  checkDate():boolean
+  {
+    this.noError = this.date? true: false;
+    return this.noError;
+  }
   onCurrencyNameChange(event:Event)
   {
+    this.baseCurrency = (<HTMLInputElement>event.target).value;
     console.log((<HTMLInputElement>event.target).value);
   }
 
   onDateChange(event:Event)
   {
+    this.date = (<HTMLInputElement>event.target).value;
     console.log((<HTMLInputElement>event.target).value);
+    this.checkDate();
   }
 
   displayCurrencyInfo(event:Event)
   {
+    if(this.checkDate())
+    {
+      
+    }
     // [] - из компонента в шаблон
     // () - из шаблона в компонент
     //(<HTMLInputElement>event.target).value
